5bb1f84e0900890843d62104335163fa94937295
Ansumana Mansaray <ansumanam@brifatsenenterprise.com>
2025-11-27 06:13:06 +0000

    File changes

--- src/pages/Locations.jsx
+++ src/pages/Locations.jsx
@@ -24,6 +24,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Badge } from "@/components/ui/badge";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
   Dialog,
   DialogContent,
