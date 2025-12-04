88197106d0a3e39bfa5b5caf8924aeb4d13bf5bb
Ansumana Mansaray <ansumanam@brifatsenenterprise.com>
2025-12-04 00:52:50 +0000

    File changes

--- src/components/hr/PayslipGenerator.jsx
+++ src/components/hr/PayslipGenerator.jsx
@@ -337,6 +337,8 @@ export default function PayslipGenerator({ payroll, employee, organisation }) {
         </head>
         <body>
           <div class="document">
+            ${organisation?.code ? `<div class="watermark">${organisation.code}</div>` : ''}
+            ${organisation?.code ? `<div class="org-code-badge">CODE: ${organisation.code}</div>` : ''}
             ${getUnifiedHeader(organisation, 'Payslip', format(new Date(payroll?.period_start), 'MMMM yyyy'), `${frequency.charAt(0).toUpperCase() + frequency.slice(1)} Pay`, 'payslip')}
             
             <div class="employee-info">
