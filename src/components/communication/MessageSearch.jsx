e1e47a633af47f42d8f1a4f1c1ac6a3299477942
Ansumana Mansaray <ansumanam@brifatsenenterprise.com>
2025-12-09 20:23:03 +0000

    File changes

--- src/components/communication/MessageSearch.jsx
+++ src/components/communication/MessageSearch.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { base44 } from "@/api/base44Client";
 import { format } from "date-fns";
 import { debounce } from "lodash";
